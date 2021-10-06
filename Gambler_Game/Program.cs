70ec5f47aed05d7dde96e6f5ed523423764bc0ce
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-06 09:55:22 +0530

    Welcome

---
+++ Gambler_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gambler_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Problem");
+        }
+    }
+}
