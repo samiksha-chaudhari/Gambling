fda62b2374bd2cf79d0af5d54f21337741f964c0
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-06 10:26:07 +0530

    start beating

---
+++ Gambler_Game/Gamblers_Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler_Game
+{
+    class Gamblers_Solution
+    {
+        
+        public void start()
+        {
+            int money = 100;
+            int bet = 1;
+            Console.WriteLine("Total money is :" +money);
+            money = money - bet;
+            Console.WriteLine("After beati money is :" +money);
+
+        }
+    }
+}
--- Gambler_Game/Program.cs
+++ Gambler_Game/Program.cs
@@ -6,7 +6,29 @@ namespace Gambler_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Gambling Problem");
+            
+            int defcount = 0;//counter to break the while loop
+
+            while (defcount == 0)
+            {
+                Console.WriteLine("------------------------Welcome to Gambling Problem-----------------------");
+                Console.WriteLine("1.Start Beating  2.Exit");
+                Console.WriteLine("Choose Option :");
+                int choice = Convert.ToInt32(Console.ReadLine());//variable for taking choice from the user
+
+                switch (choice)  //switch case
+                {
+                    case 1:
+                        Gamblers_Solution game = new Gamblers_Solution();
+                        game.start();
+                        break;
+                             
+                    default:
+                        Console.WriteLine("End");//default condition
+                        defcount++;
+                        break;
+                }
+            }
         }
     }
 }
