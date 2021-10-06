649ee93cb73cab94d4cd3e5a10f06538fe7082b7
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-06 12:29:22 +0530

    updating

--- Gambler_Game/Gamblers_Solution.cs
+++ Gambler_Game/Gamblers_Solution.cs
@@ -15,34 +15,35 @@ namespace Gambler_Game
         /// </summary>
         public void start()
         {
-            int win = 0;
-            int loss = 0;
+            int win = 100;
+            int loss = 100;
             Console.WriteLine("Total money :" +money);
             
             //cheating random variable
             Random random = new Random();
-            int win1 = 100;
-            int loss1 = 100;
+            
 
             //condition to check person winning or loss
-            while (win1 ==150 ||loss1 ==50)
+            while (win != 150 || loss != 50)
             {
-                int toss = random.Next(0, 2);
-                if (toss == 0)
-                {
-                    win += bet;
-                    win1++;
-                    Console.WriteLine($"Total money after winnig is : " +win1);
-                }
-                else if (toss == 1)
-                {
-                    loss -= bet;
-                    loss1--;
-                    Console.WriteLine($"Total money after lossing is : " +loss1);
-                }
-               // Console.WriteLine("Remaing money" + money);
+               
+                    int toss = random.Next(0, 2);
+                    if (toss == 0)
+                    {
+                        win += bet;
+                       
+                        Console.WriteLine($"Total money after winnig is : " + win);
+                    }
+                    else if (toss == 1)
+                    {
+                        loss -= bet;
+                        
+                        Console.WriteLine($"Total money after lossing is : " + loss);
+                    }
+                    // Console.WriteLine("Remaing money" + money);
+
+               
             }
-            
         }
     }
 }
